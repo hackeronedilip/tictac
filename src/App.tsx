d05e36a80958f4d979db2e4ac76bbabaeb609596
kishorebaskr <238240319+kishorebaskr@users.noreply.github.com>
2025-10-24 21:12:23 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -99,9 +99,9 @@ function App() {
         <div className="p-6 bg-indigo-600 text-white text-center">
           <h1 className="text-3xl font-bold flex items-center justify-center gap-2">
             <Award className="h-8 w-8" />
-            Dilip TicTac Game
+            Kirubakaran TicTac Game
           </h1>
-          <p className="text-indigo-200 mt-1">Devops Game</p>
+          <p className="text-indigo-200 mt-1">Oct Devops Game</p>
         </div>
         
         <div className="p-6 md:p-8 grid grid-cols-1 md:grid-cols-3 gap-8">
